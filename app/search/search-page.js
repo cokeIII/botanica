5df9bebcd07fbd000399e65c23aadfd9b0c27bd6
cokeIII <coke2484@gmail.com>
2020-12-10 23:26:44 +0700

    search

--- app/search/search-page.js
+++ app/search/search-page.js
@@ -1,8 +1,48 @@
-const SearchViewModel = require("./search-view-model");
+var Observable = require("data/observable")
+const httpModule = require("tns-core-modules/http")
+var API_URL = "http://10.0.5.7:7777"
+var serverImg = "https://rms.chontech.ac.th/server_botanica/plant/"
+var pageData = new Observable.fromObject({
+  trees: [],
+  searchPhrase: "",
+  bgColor: true,
+  sverPath: serverImg,
+})
+let page;
+exports.pageLoaded = function (args) {
+    console.log("pageLoaded")
+    page = args.object
+    page.bindingContext = pageData
+    fetch(API_URL + "/getPlant", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+    }).then((r) => r.json()).then((response) => {
+        console.log(response.data)
+        pageData.trees = response.data;
+    }).catch((e) => {
+        console.log('***fetch error***')
+    });
 
-function onNavigatingTo(args) {
-    const component = args.object;
-    component.bindingContext = new SearchViewModel();
 }
 
-exports.onNavigatingTo = onNavigatingTo;
+exports.onSearchSubmit = function () {
+
+}
+exports.onItemTap = function (args) {
+    const view = args.view;
+    const page = view.page;
+    const tappedItem = view.bindingContext;
+
+    page.frame.navigate({
+        moduleName: "search/search-item-detail/search-item-detail-page",
+        context: tappedItem,
+        animated: true,
+        transition: {
+            name: "slide",
+            duration: 200,
+            curve: "ease"
+        }
+    });
+}
+
