9cff8247c8cb3f2f49ba4c91933ef3f654a27d3e
cokeIII <coke2484@gmail.com>
2020-12-12 00:23:11 +0700

    map

--- app/home/home-items-page.js
+++ app/home/home-items-page.js
@@ -1,26 +1,65 @@
-const HomeItemsViewModel = require("./home-items-view-model");
+var Observable = require("data/observable")
+const httpModule = require("tns-core-modules/http")
+var API_URL = "http://10.0.5.7:7777"
+var serverImg = "https://rms.chontech.ac.th/server_botanica/plant/"
+const labelModule = require("tns-core-modules/ui/label")
+require("nativescript-dom")
 
-function onNavigatingTo(args) {
-    const component = args.object;
-    component.bindingContext = new HomeItemsViewModel();
+var pageData = new Observable.fromObject({
+    map:[],
+})
+fetch(API_URL + "/getMap", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({})
+}).then((r) => r.json()).then((response) => {
+    pageData.map = response.data;
+}).catch((e) => {
+    console.log('***fetch error***')
+});
+
+function romoveMap() {
+    mapR = page.getViewById("map")
+    viewMap = mapR.getElementsByClassName('point')
+    for(let i = 0;i<viewMap.length;i++){
+        mapR.removeChild(viewMap[i])
+    }
+    txtMap = mapR.getElementsByClassName('txt-map')
+    for(let i = 0;i<viewMap.length;i++){
+        mapR.removeChild(txtMap[i])
+    }
+
 }
 
-function onItemTap(args) {
-    const view = args.view;
-    const page = view.page;
-    const tappedItem = view.bindingContext;
-
-    page.frame.navigate({
-        moduleName: "home/home-item-detail/home-item-detail-page",
-        context: tappedItem,
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 200,
-            curve: "ease"
-        }
-    });
+exports.pageLoaded = function (args) {
+    console.log("pageLoaded")
+    page = args.object
+    page.bindingContext = pageData
+    map = page.getViewById("map")
+    romoveMap()
+    if(Object.keys(pageData.map).length !== 0){
+        console.log(pageData.map)
+        
+        pageData.map.forEach(element => {
+            genPoint(element.x,element.y,element.uuid,element.name);
+        });
+    }
+    // viewMap = map.getElementsByClassName('point')
 }
+function genPoint(x,y,id,name){
+    let myLabel = new labelModule.Label()
+    let myLabelText = new labelModule.Label()
 
-exports.onItemTap = onItemTap;
-exports.onNavigatingTo = onNavigatingTo;
+    myLabel.className = "point"
+    myLabel.id = id
+    myLabel.marginTop = ""+y-5+"%"
+    myLabel.marginLeft = ""+x+"%"
+    myLabel.style.zIndex="-1"
+    myLabelText.text = name
+    myLabelText.class = "txt-map"
+    myLabelText.marginLeft = ""+x+"%"
+    myLabelText.marginTop = ""+(y-5)+"%"
+    map.addChild(myLabel)
+    map.addChild(myLabelText)
+    console.log(map)
+}
--- app/search/search-page.js
+++ app/search/search-page.js
@@ -3,31 +3,51 @@ const httpModule = require("tns-core-modules/http")
 var API_URL = "http://10.0.5.7:7777"
 var serverImg = "https://rms.chontech.ac.th/server_botanica/plant/"
 var pageData = new Observable.fromObject({
-  trees: [],
-  searchPhrase: "",
-  bgColor: true,
-  sverPath: serverImg,
-})
-let page;
+    trees: [],
+    searchPhrase: "",
+    bgColor: true,
+    sverPath: serverImg,
+    countries : [],
+  })
+
+
+fetch(API_URL + "/getPlant", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({})
+}).then((r) => r.json()).then((response) => {
+    pageData.trees = response.data;
+    response.data.forEach(element => {
+        pageData.countries.push(element.plan_name)
+    });
+}).catch((e) => {
+    console.log('***fetch error***')
+});
+
+let page
+let myControl
 exports.pageLoaded = function (args) {
     console.log("pageLoaded")
     page = args.object
     page.bindingContext = pageData
-    fetch(API_URL + "/getPlant", {
+    myControl = page.getViewById("myControl")
+}
+
+exports.serachData = function () {
+    let val = myControl.selectedValue
+    console.log(val+"")
+    fetch(API_URL + "/searchData", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({})
+        body: JSON.stringify({
+            sKey: val+'',
+        })
     }).then((r) => r.json()).then((response) => {
         console.log(response.data)
         pageData.trees = response.data;
     }).catch((e) => {
         console.log('***fetch error***')
     });
-
-}
-
-exports.onSearchSubmit = function () {
-
 }
 exports.onItemTap = function (args) {
     const view = args.view;
